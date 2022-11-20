cb414f420c4968b8bfc082af05541ba9076e9a0d
DeveshS02 <97830736+DeveshS02@users.noreply.github.com>
2022-11-20 23:14:50 +0530

    Add files via upload

---
+++ userSuperClass.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+class userSuperClass {
+
+    //Member Variables
+    private String name;
+    private String userName;
+    private String password;
+    private ArrayList<Integer> cart; //Change Datatype to product later
+    private String phoneNumber;
+    private String address;
+}
